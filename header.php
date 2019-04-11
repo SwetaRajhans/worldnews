9c857b8aa9822c136b8e7dbec4506998856d90e7
unknown <swetarajhans935@gmail.com>
2019-04-11 22:01:30 +0530

    Index page updated

---
+++ header.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>The World - News &amp; Lifestyle Magazine</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+
+    <!-- Core Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+
+        <!-- Top Header Area -->
+        <div class="top-header-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="top-header-content d-flex align-items-center justify-content-between">
+                            <!-- Logo -->
+                            <div class="logo">
+                                <a href="index.html"><h1 style="color:white"><i>The World News</i></h1></a>
+                            </div>
+
+                            <!-- Login Search Area -->
+                            <div class="login-search-area d-flex align-items-center">
+                                <!-- Login -->
+                                <div class="login d-flex">
+                                    <a href="#">Login</a>
+                                    <a href="#">Register</a>
+                                </div>
+                                <!-- Search Form -->
+                                <div class="search-form">
+                                    <form action="#" method="post">
+                                        <input type="search" name="search" class="form-control" placeholder="Search">
+                                        <button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Navbar Area -->
+        <div class="newspaper-main-menu" id="stickyMenu">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="newspaperNav">
+
+                        <!-- Logo -->
+                        <div class="logo">
+                            <a href="index.html"><img src="img/core-img/logo.png" alt=""></a>
+                        </div>
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- close btn -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li class="active"><a href="index.html">Home</a></li>
+                                    <li><a href="#">Menu</a>
+                                        <div class="megamenu">
+                                            <ul class="single-mega cn-col-4">
+                                                <li class="title">Sports</li>
+                                                <li><a href="index.html">Cricket</a></li>
+                                                <li><a href="catagories-post.html">Football</a></li>
+                                                <li><a href="single-post.html">Hockey</a></li>
+                                                <li><a href="about.html">Tennis</a></li>
+                                                <li><a href="contact.html">WWE</a></li>
+                                            </ul>
+                                            <ul class="single-mega cn-col-4">
+                                                <li class="title">Entertainment</li>
+                                                <li><a href="index.html">Bollywood</a></li>
+                                                <li><a href="catagories-post.html">Hollywood</a></li>
+                                                <li><a href="single-post.html">TV Series</a></li>
+                                                <li><a href="about.html">Web Series</a></li>
+                                                <li><a href="contact.html">Regional</a></li>
+                                            </ul>
+                                            <ul class="single-mega cn-col-4">
+                                                <li class="title">Technology</li>
+                                                <li><a href="index.html">Smartphone</a></li>
+                                                <li><a href="catagories-post.html">Information Technology</a></li>
+                                                <li><a href="single-post.html">Artifical Intelligence</a></li>
+                                                <li><a href="about.html">Robotics</a></li>
+                                                <li><a href="contact.html">Space Technology</a></li>
+                                            </ul>
+                                            <ul class="single-mega cn-col-4">
+                                                <li class="title">Others</li>
+                                                <li><a href="index.html">Politics</a></li>
+                                                <li><a href="catagories-post.html">Business</a></li>
+                                                <li><a href="single-post.html">Health</a></li>
+                                                <li><a href="about.html">Travel</a></li>
+                                                <li><a href="contact.html">Agriculture</a></li>
+                                            </ul>
+                                            <div class="single-mega cn-col-4">
+                                                <!-- Single Featured Post -->
+                                                <div class="single-blog-post small-featured-post d-flex">
+                                                    <div class="post-thumb">
+                                                        <a href="#"><img src="img/bg-img/23.jpg" alt=""></a>
+                                                    </div>
+                                                    <div class="post-data">
+                                                        <a href="#" class="post-catagory">Travel</a>
+                                                        <div class="post-meta">
+                                                            <a href="#" class="post-title">
+                                                                <h6>Pellentesque mattis arcu massa, nec fringilla turpis eleifend id.</h6>
+                                                            </a>
+                                                            <p class="post-date"><span>7:00 AM</span> | <span>April 14</span></p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <!-- Single Featured Post -->
+                                                <div class="single-blog-post small-featured-post d-flex">
+                                                    <div class="post-thumb">
+                                                        <a href="#"><img src="img/bg-img/24.jpg" alt=""></a>
+                                                    </div>
+                                                    <div class="post-data">
+                                                        <a href="#" class="post-catagory">Politics</a>
+                                                        <div class="post-meta">
+                                                            <a href="#" class="post-title">
+                                                                <h6>Augue semper congue sit amet ac sapien. Fusce consequat.</h6>
+                                                            </a>
+                                                            <p class="post-date"><span>7:00 AM</span> | <span>April 14</span></p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li><a href="#">Politics</a></li>
+                                    <li><a href="#">Breaking News</a></li>
+                                    <li><a href="#">Business</a></li>
+                                    <li><a href="#">Technology</a></li>
+                                    <li><a href="#">Health</a></li>
+                                    <li><a href="#">Travel</a></li>
+                                    <li><a href="#">Sports</a></li>
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Hero Area Start ##### -->
+    <div class="hero-area">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-12 col-lg-8">
+                    <!-- Breaking News Widget -->
+                    <div class="breaking-news-area d-flex align-items-center">
+                        <div class="news-title">
+                            <p>Breaking News</p>
+                        </div>
+                        <div id="breakingNewsTicker" class="ticker">
+                            <ul>
+                                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+                                <li><a href="#">Welcome to Colorlib Family.</a></li>
+                                <li><a href="#">Nam eu metus sitsit amet, consec!</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- Breaking News Widget -->
+                    <div class="breaking-news-area d-flex align-items-center mt-15">
+                        <div class="news-title title2">
+                            <p>International</p>
+                        </div>
+                        <div id="internationalTicker" class="ticker">
+                            <ul>
+                                <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+                                <li><a href="#">Welcome to Colorlib Family.</a></li>
+                                <li><a href="#">Nam eu metus sitsit amet, consec!</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Hero Add -->
+                
+            </div>
+        </div>
+    </div>
